793ad5e2532aa5af96339687819357e9e4102edf
emylinsousa <emylin.sousa@gmail.com>
2014-04-21 13:49:23 -0500

    arrows/child

--- app/controllers/restaurant.js
+++ app/controllers/restaurant.js
@@ -7,26 +7,14 @@
 var APP = require("core");
 
 var CONFIG = arguments[0];
-
+//
+$.NavigationBar.showBack(function(_event) {
+	APP.removeChild();
+});
+//
 APP.log("debug", "text | " + JSON.stringify(CONFIG));
 
 $.NavigationBar.setBackgroundColor(APP.Settings.colors.primary);
-
-if(CONFIG.isChild === true) {
-	$.NavigationBar.showBack(function(_event) {
-		APP.removeChild();
-	});
-} else {
-	if(APP.Settings.useSlideMenu) {
-		$.NavigationBar.showMenu(function(_event) {
-			APP.toggleMenu();
-		});
-	} else {
-		$.NavigationBar.showSettings(function(_event) {
-			APP.openSettings();
-		});
-	}
-}
 
 $.menu.addEventListener("click", function() {
 	APP.addChild("menu", {
